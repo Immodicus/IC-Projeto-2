68933772cf1fda743064d85858b4402e9d47fbd0
Immodicus <53706492+Immodicus@users.noreply.github.com>
2022-10-31 14:56:04 +0000

    img_cpy

--- img_cpy.cpp
+++ img_cpy.cpp
@@ -4,7 +4,34 @@
 #include <iostream>
 
 using namespace cv;
-int main()
+int main(int argc, char** argv)
 {
-    
+    if(argc < 3)
+    {
+        std::cerr << "Invalid usage. Usage: img_cpy original copy\n";
+        return 1;
+    }
+
+    std::string image_path = samples::findFile(argv[argc-2]);
+
+    Mat img = imread(image_path, IMREAD_ANYCOLOR);
+    if(img.empty())
+    {
+        std::cout << "Could not read the image: " << image_path << std::endl;
+        return 1;
+    }
+
+    Rect r (0, 0, img.cols, img.rows);
+    Mat out = img(r);
+
+    for(size_t i = 0; i < img.cols; i++)
+    {
+        for(size_t j = 0; j < img.rows; j++)
+        {            
+            img.at<Vec3b>(i, j) = img.at<Vec3b>(i, j);
+        }
+    }
+
+    imwrite(argv[argc-1], out);
+    return 0;
 }
