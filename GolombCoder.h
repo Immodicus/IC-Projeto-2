b54a9c28c39125e5b8eae11127689530aeb172b0
Immodicus <53706492+Immodicus@users.noreply.github.com>
2022-11-09 00:58:09 +0000

    Golomb Coder support for Bitstream decoding

--- GolombCoder.h
+++ GolombCoder.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -6,34 +7,19 @@
 #include <iostream>
 #include <string.h>
 #include "BitSet.h"
+#include "BitStream.h"
 
+#define BREAK(expr) if(!expr) goto end;
 
 class GolombCoder
 {
 private:
-    bool GetBit(int64_t c, int64_t pos)
+    static bool GetBit(uint64_t c, uint64_t pos)
     {
         return (c >> (63 - (pos % 64))) & 1;
     }
 
-    static bool SGetBit(int64_t c, int64_t pos)
-    {
-        return (c >> (63 - (pos % 64))) & 1;
-    }
-
-    void SetBit(int64_t& c, int64_t pos, bool value) const
-    {
-        if (value)
-        {
-            c |= (1 << (63 - pos));
-        }
-        else
-        {
-            c &= ~(1 << (63 - pos));
-        }
-    }
-
-    static void SSetBit(uint64_t& c, uint64_t pos, bool value)
+    static void SetBit(uint64_t& c, uint64_t pos, bool value)
     {
         if (value)
         {
@@ -49,8 +35,8 @@ private:
 public:
     static BitSet Encode(int64_t i, uint64_t m)
     {
-        int64_t q = i / m;
-        uint64_t r = i % m;
+        uint64_t q = abs(i) / m;
+        uint64_t r = abs(i) % m;
 
         uint64_t b = std::floor(log2(static_cast<double>(m)));
         
@@ -77,7 +63,7 @@ public:
         {
             for(size_t k = q+1, h = b-1; k < (q+1)+b; k++, h--)
             {
-                bs.SetBit(k, SGetBit(r, 63-h));
+                bs.SetBit(k, GetBit(r, 63-h));
             }
         }
         else // code the number r + 2 b + 1 − M {\displaystyle r+2^{b+1}-M} {\displaystyle r+2^{b+1}-M} in binary representation using b + 1 bits.
@@ -86,7 +72,7 @@ public:
 
             for(size_t k = q+1, h = b; k < (q+1)+b+1; k++, h--)
             {
-                bs.SetBit(k, SGetBit(r, 63-h));
+                bs.SetBit(k, GetBit(r, 63-h));
             }
         }
 
@@ -108,16 +94,58 @@ public:
 
         for(size_t i = 0; i < b; i++)
         {
-            SSetBit(r, 63-b+i+1, bs[q+1+i]);
+            SetBit(r, 63-b+i+1, bs[q+1+i]);
         }
 
         if(r >= pow(2, b+1) - m)
         {
             r <<= 1;
-            SSetBit(r, 63, bs[bs.size()-1]);
+            SetBit(r, 63, bs[bs.size()-1]);
             r = r - pow(2, b+1) + m;
         }
 
         return q * m + r;
+    }
+
+    static std::vector<int64_t> Decode(BitStream& bs, uint64_t m)
+    {
+        std::vector<int64_t> decoded(0);
+
+        uint64_t b = std::floor(log2(static_cast<double>(m)));
+        
+        while(!bs.End())
+        {
+            bool bit = false;
+            
+            uint64_t q = 0;
+
+            for(; q < UINT64_MAX; q++)
+            {
+                BREAK(bs.ReadBit(bit));
+                if(bit) break;
+            }
+
+            uint64_t r = 0;
+
+            for(size_t i = 0; i < b; i++)
+            {
+                BREAK(bs.ReadBit(bit));
+                SetBit(r, 63-b+i+1, bit);
+            }
+
+            if(r >= pow(2, b+1) - m)
+            {
+                r <<= 1;
+                BREAK(bs.ReadBit(bit));
+                SetBit(r, 63, bit);
+                r = r - pow(2, b+1) + m;
+            }
+
+            // std::cout << "push: " << q * m + r << std::endl;
+            decoded.push_back(q * m + r);
+        }
+
+    end:
+        return decoded;
     }
 };
--- golomb_test.cpp
+++ golomb_test.cpp
@@ -1,4 +1,5 @@
 #include "GolombCoder.h"
+#include "BitStream.h"
 #include <stdio.h>
 
 int main()
@@ -12,6 +13,31 @@ int main()
             assert(n == GolombCoder::Decode(b, m));
         }
         std::cout << "m: " << m << " OK\n";
+    }
+
+    BitStream* out = new BitStream("test.col", "w+");
+
+    int64_t a = 45-50;
+    uint64_t b = 453;
+    uint64_t c = 136453;
+    uint64_t d = 5000000;
+
+    uint64_t m = 256;
+
+    out->WriteNBits(GolombCoder::Encode(a, m));
+    out->WriteNBits(GolombCoder::Encode(b, m));
+    out->WriteNBits(GolombCoder::Encode(c, m));
+    out->WriteNBits(GolombCoder::Encode(d, m));
+
+    delete out;
+
+    BitStream in("test.col", "r");
+
+    auto result = GolombCoder::Decode(in, m);
+
+    for(auto d : result)
+    {
+        std::cout << d << "\n";;
     }
 
     return EXIT_SUCCESS;
