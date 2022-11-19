3a3ac4d303096c3d5af338d5901a5a850f17a386
Jodionio Muachifi <jodionisiomuachifi@ua.pt>
2022-11-19 19:01:37 +0000

    lossless/lossy is working - segmentation fault fixed

--- draft-code/AudioCodec.hpp
+++ draft-code/AudioCodec.hpp
@@ -5,56 +5,62 @@
 #include <sndfile.h>
 #include <vector>
 
+/**
+ * Class for encoding and decoding audio files.
+ */
+
 class AudioCodec
 {
 private:
-    /* data */
-    char *fname;
+    char *filename;
     SF_INFO sfinfo;
-    int num_in;
+    int ninput;
     std::vector<short> chs = {};
     std::vector<short> rn = {};
 
 public:
     AudioCodec();
-    /**
-     * @brief Construct a new Audio Codec object
-     *
-     * @param fname, represent a path store filename
-     */
-    AudioCodec(const char *fname);
 
     /**
-     *Compress Audio File
-     * @brief
-     * @param fDst repperesent the path store the filename destination
-     * @param num to choose the order of the predictor
-     * @param lossy to choose between lossless(0) and lossy(1) encoding
-     * @param shamt represent the number of bits to be quantized in the predictor
+     * AudioCodec Class Constructor
+     * @param filename path to an audio file.
      */
-    void compress(const char *fDst, int num, bool lossy, int shamt);
+    AudioCodec(const char *filename);
 
     /**
-     *Decompress Audio File
-     * @brief
-     * @param fSrc repperesent the path store the filename source
+     * Compress audio file
+     * @param filename path to a file were to store the enconded value.
+     * @param num value to choose the order of the predictor that will be use.
+     * Choosing 1, the predictor will only consider the previous value; 2 it will consider the 2 previous values; and choosing 3 will consider the 3 previous values.
+     * @param lossy value to choose between lossless (0) and lossy (1) encoding.
+     * @param shamt number of bits to be quantized in the predictor
      */
-    void decompress(const char *fSrc);
+    void compress(const char *fileDst, int num, bool lossy, int shamt);
 
     /**
-     * Lossless Predictor
-     * @brief
-     * @param vectSample is the vector that contains all samples
+     * Lossless Preditor
+     * @param vetSrc vector that contains all the samples of the audio file.
      */
-    void losslessPredictor(std::vector<short> vectSample);
+    void preditor(std::vector<short> vetSrc);
 
     /**
-     * Lossy Predictor
-     * @brief
-     * @param vectSample is the vector that contains all samples
-     * @param shamt describe the number of bits to be quantized in the predictor
+     * Lossy Preditor
+     * @param vetSrc vector that contains all the samples of the audio file.
+     * @param shamt number of bits to be quantized in the predictor
      */
-    void lossyPredictor(std::vector<short> vectSample, int shamt);
+    void preditorLossy(std::vector<short> vetSrc, int shamt);
+
+    /**
+     * Decompress audio file
+     * @param fileSrc path to a file were the stored enconded value is.
+     */
+    void decompress(const char *fileSrc);
+
+    /**
+     * Show the histograms of the residuals obtained after prediction, as well as the entropy value.
+     * Compare the values with the entropy of the original data.
+     */
+    void showHistEnt();
 };
 
 #endif
